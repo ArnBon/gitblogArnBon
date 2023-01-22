8d40d14ba55f28531d362c857e04513f4e75b2ff
Arnaldo Bonillo <bonillaarnaldo@gmail.com>
2023-01-22 15:09:30 -0400

    Video 4

--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon; /*se coloco en el video 4*/
 
 class Post extends Model
 {
-    //
+    protected $dates = ['published_at']; /*se coloco en el video 4 por defecto laravel trata los campos fechas como instancia de carbon*/
 }
