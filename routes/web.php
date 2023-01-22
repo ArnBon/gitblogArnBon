94d9697384361fa1083ce524e306d4ecc22f6b1f
Arnaldo Bonillo <bonillaarnaldo@gmail.com>
2023-01-22 14:59:13 -0400

    Video 3

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,26 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+//video 3
+// Route::get('posts', function(){
+//     return App\Post::all();
+// }); //esto fue modificado por el de abajo
+
+// Route::get('/', function(){
+//     $posts = App\Post::all();
+//     return view('welcome', compact('posts'));
+// }); // este fue modificado por el de abajop
+
+Route::get('/', function(){
+    $posts = App\Post::latest('published_at')->get();
+    return view('welcome', compact('posts'));
 });
+
+Route::get('posts', function(){
+    return App\Post::all();
+}); 
+//*fin video 3*
