f094a90adc39c4daf18d45c75be6f636304edcff
Arnaldo Bonillo <bonillaarnaldo@gmail.com>
2023-01-23 19:51:19 -0400

    Video 14

--- routes/web.php
+++ routes/web.php
@@ -89,7 +89,13 @@ Route::group([
     'middleware' => 'auth'],
     function(){
         Route::get('/', 'AdminController@index')->name('dashboard');
-        Route::get('posts', 'PostsController@index')->name('admin.posts.index');        
+        Route::get('posts', 'PostsController@index')->name('admin.posts.index');         
+        Route::get('posts/create','PostsController@create')->name('admin.posts.create');/**Video 14 */
+             
     });
 /**fin video 12 */
+
+
+
+
 
