ba28661e3eabf9dfaae22301b00bdc56375ec55c
Arnaldo Bonillo <bonillaarnaldo@gmail.com>
2023-01-27 19:34:53 -0400

    Video 20

--- routes/web.php
+++ routes/web.php
@@ -1,51 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-//video 3
-// Route::get('posts', function(){
-//     return App\Post::all();
-// }); //esto fue modificado por el de abajo
-
-// Route::get('/', function(){
-//     $posts = App\Post::all();
-//     return view('welcome', compact('posts'));
-// }); // este fue modificado por el de abajop
-
-/**comentado en el video 11 */
-// Route::get('/', function(){
-//     $posts = App\Post::latest('published_at')->get();
-//     return view('welcome', compact('posts'));
-// });
-/**fin comentado en el video 11 */
-
-Route::get('posts', function(){
-    return App\Post::all();
-}); 
-//*fin video 3*
-
-/**video 8 */
-// Route::get('admin', function(){
-//     return view('admin.dashboard');
-// });
-/**fin video 8 */
-
-/*video 9*/
-//Route::auth(); se reemplaza con el contenido del archivo Router.php el de abajo
-// Authentication Routes... esto se hace para personalizar el login y registro//
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
@@ -54,35 +8,19 @@ Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 // Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
 // Route::post('register', 'Auth\RegisterController@register');
 
-// Password Reset Routes...
+
 Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
-/**fin video 9 */
 
-/** Video 10 se cambia admin por home y se agrega un middleware referencia video 8*/
-// Route::get('home', function(){
-//     return view('admin.dashboard');
-// })->middleware('auth');
-/**fin video 10 */
 
-/*video 11*/
+/*Rutas publicas*/
 Route::get('/', 'PagesController@home');
-// Route::get('home', 'HomeController@index'); comentado en el video 13
-// Route::get('admin/posts', 'Admin\PostsController@index');
-Route::get('admin', 'HomeController@index'); //colocado y luego comentado en video 13 porque se movio al grupo de rutas
-/**despues vino la creacion de los grupos de rutas */
-// Route::group([
-// 'prefix'     => 'admin',
-// 'namespace'  => 'Admin',
-// 'middleware' => 'auth'],
-// function(){
-//     Route::get('posts', 'PostsController@index');
-// });
-/**fin video 11 */
+Route::get('blog/{id}', 'PostsController@show');
+Route::get('admin', 'HomeController@index'); 
 
-/**video 12  le agregamos un nombre a la ruta*/
+/*Rutas para la administración*/
 Route::group([
     'prefix'     => 'admin',
     'namespace'  => 'Admin',
@@ -90,11 +28,10 @@ Route::group([
     function(){
         Route::get('/', 'AdminController@index')->name('dashboard');
         Route::get('posts', 'PostsController@index')->name('admin.posts.index');         
-        Route::get('posts/create','PostsController@create')->name('admin.posts.create');/**Video 14 */
-        Route::post('posts', 'PostsController@store')->name('admin.posts.store'); //video 17
-             
+        Route::get('posts/create','PostsController@create')->name('admin.posts.create');
+        Route::post('posts', 'PostsController@store')->name('admin.posts.store'); 
     });
-/**fin video 12 */
+
 
 
 
