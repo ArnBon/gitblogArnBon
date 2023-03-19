1c8421a2c531069f88c0852c0f38251cadcb996a
Arnaldo Bonillo <bonillaarnaldo@gmail.com>
2023-03-19 08:49:27 -0400

    Video 73

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,7 +4,12 @@
     <section class="pages container">
     <div class="page page-about">
         <h1 class="text-capitalize">Página no encontrada</h1>
-        <p>Regresar a <a href="{{ route('pages.home') }}">Inicio</a></p>
+        <span style="color:red">{{ $exception->getMessage() }}</span>
+
+        {{-- <p>Regresar a <a href="{{ route('pages.home') }}">Inicio</a></p> --}}
+        <p><a href="{{ url()->previous() }}">Regresar</a></p>
+
+
     </div>
 </section>
 @endsection
