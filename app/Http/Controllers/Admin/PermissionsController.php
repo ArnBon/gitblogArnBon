b784461c544eed14ba63d16e079c9488b159c237
Arnaldo Bonillo <bonillaarnaldo@gmail.com>
2023-03-21 16:07:19 -0400

    Video 76

--- app/Http/Controllers/Admin/PermissionsController.php
+++ app/Http/Controllers/Admin/PermissionsController.php
@@ -9,14 +9,18 @@ use Spatie\Permission\Models\Permission;
 class PermissionsController extends Controller
 {
     public function index()
-    {        
+    {  
+    $this->authorize('view', new Permission);
+        
         return view('admin.permissions.index',[
             'permissions' => Permission::all()
         ]);
     }
 
     public function edit(Permission $permission)
-    {   
+    {
+        $this->authorize('update', $permission); 
+
         return view('admin.permissions.edit', [
             'permission' => $permission
         ]);
@@ -24,6 +28,8 @@ class PermissionsController extends Controller
 
     public function update(Request $request, Permission $permission)
     {  
+        $this->authorize('update', $permission); 
+
         $data = $request->validate(['display_name' => 'required']);
 
         $permission->update($data);
